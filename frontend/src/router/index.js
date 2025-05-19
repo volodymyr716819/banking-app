30c034119b2b2a6545ae5f201c4cc12d3ac99cd2
volodymyr716819 <vo.gulchenko@gmail.com>
2025-05-19 15:49:56 +0200

    fix: transaction view router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,9 +7,10 @@ import AccountsView from "../views/AccountsView.vue";
 import DashboardLayout from "../layout/DashboardLayout.vue";
 import TransferPageView from "../views/TransferView.vue";
 import ApproveAccountsView from "../views/ApproveAccountsView.vue";
-import ApproveUsersView from "../views/ApproveUsersView.vue"; 
-import EmployeeUsersView from '../views/EmployeeUsersView.vue';
-import EmployeeAccountsView from '../views/EmployeeAccountsView.vue';
+import ApproveUsersView from "../views/ApproveUsersView.vue";
+import EmployeeUsersView from "../views/EmployeeUsersView.vue";
+import EmployeeAccountsView from "../views/EmployeeAccountsView.vue";
+import TransactionHistoryView from "../views/TransactionHistoryView.vue"; // ✅ NEW
 
 import { useAuthStore } from "../store/auth";
 
@@ -25,6 +26,11 @@ const routes = [
       { path: "transfer", component: TransferPageView },
       { path: "atm", component: ATMView, meta: { requiresAuth: true } },
       {
+        path: "history",
+        component: TransactionHistoryView,
+        meta: { requiresAuth: true },
+      },
+      {
         path: "approve",
         component: ApproveAccountsView,
         meta: { requiresRole: "employee" },
@@ -33,15 +39,17 @@ const routes = [
         path: "approve-users",
         component: ApproveUsersView,
         meta: { requiresRole: "employee" },
-      }, 
-      { path: 'users', 
-        component: EmployeeUsersView, 
-        meta: { requiresRole: 'employee' } 
       },
-      { path: 'employee-accounts', 
-        component: EmployeeAccountsView, 
-        meta: { requiresRole: 'employee' } 
-      }
+      {
+        path: "users",
+        component: EmployeeUsersView,
+        meta: { requiresRole: "employee" },
+      },
+      {
+        path: "employee-accounts",
+        component: EmployeeAccountsView,
+        meta: { requiresRole: "employee" },
+      },
     ],
   },
   { path: "/", redirect: "/login" },
