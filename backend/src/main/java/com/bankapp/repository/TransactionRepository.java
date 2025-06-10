b0fdfc5ccf08256ae8df15941fae70d5c6ef3e26
panagiotistri <144345813+panagiotistri@users.noreply.github.com>
2025-06-10 20:43:02 +0200

    iban transfer fix

--- backend/src/main/java/com/bankapp/repository/TransactionRepository.java
+++ backend/src/main/java/com/bankapp/repository/TransactionRepository.java
@@ -3,7 +3,9 @@ package com.bankapp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import com.bankapp.model.Account;
 import com.bankapp.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
@@ -11,4 +13,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     List<Transaction> findByFromAccount_User_IdOrToAccount_User_Id(Long fromUserId, Long toUserId);
     
     List<Transaction> findByFromAccount_IdOrToAccount_Id(Long fromAccountId, Long toAccountId);
+
+    Optional<Account> findByIban(String iban);
 }
