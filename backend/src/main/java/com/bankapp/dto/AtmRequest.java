7803e899923c212e50a6b6195db0263710e1460f
DenisaaMinu <minudenisa29@gmail.com>
2025-06-24 18:22:49 +0200

    atmRequest fix

--- backend/src/main/java/com/bankapp/dto/AtmRequest.java
+++ backend/src/main/java/com/bankapp/dto/AtmRequest.java
@@ -1,1 +1,45 @@
-??
+package com.bankapp.dto;
+import com.bankapp.model.AtmOperation.OperationType;
+
+import java.math.BigDecimal;
+
+public class AtmRequest {
+
+    private Long accountId;
+    private BigDecimal amount;
+    private char[] pin;
+    private OperationType operationType;
+
+    // Getters and Setters
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    
+    public char[] getPin() {
+        return pin;
+    }
+
+    public void setPin(char[] pin) {
+        this.pin = pin;
+    }
+
+    public OperationType getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(OperationType operationType) {
+        this.operationType = operationType;
+    }
+}
